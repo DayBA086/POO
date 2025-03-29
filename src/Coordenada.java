8eaeb5ab493d7dc3fa70650d251f8cf54a56f42f
DaynaBru <39018049+DaynaBru@users.noreply.github.com>
2025-03-29 18:24:08 -0500

    methods
    create methods

--- src/Coordenada.java
+++ src/Coordenada.java
@@ -1,5 +1,72 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+public class Coordenada {
+	// Atributos de la clase Coordenada
+	private double x; // Coordenada en el eje X
+	private double y; // Coordenada en el eje Y
+	private Coordenada c; // Atributo de tipo Coordenada
+
+	// Constructor por defecto, inicializa los atributos x, y a cero
+	public Coordenada( ){
+		x = 0;
+		y = 0;
+	}
+
+	// Constructor con parámetros, inicializa los atributos x, y con los valores proporcionados
+	public Coordenada(double x, double y ){
+		this.x = x;
+		this.y = y;
+	}
+
+	// Constructor con parámetro de tipo Coordenada, inicializa el atributo co con el valor proporcionado
+	public Coordenada(Coordenada c ){
+		this.c = c;
+	}
+
+	// Métodos setter para establecer los valores de x e y
+	void setx(int x) {
+		this.x = x;
+	}
+	void sety(int y){
+		this.y = y;
+	}
+
+	// Métodos getter para obtener los valores de x e y
+	double getx(){
+	 return x;
+	}
+	double gety(){
+	 return y;
+	}
+
+	// Método para el cálculo de la distancia euclideana entre esta Coordenada y otra proporcionada como parámetro
+	public double distancia(Coordenada c){
+		double Distx, Disty, distancia;
+	    Distx = c.getx();
+	    Disty = c.gety();
+	    distancia = Distx - Disty;
+	    if (distancia < 0){
+	          distancia = distancia * -1;
+	     }
+	     return distancia;
+	}
+
+	// Método de clase que calcula la distancia euclideana entre dos Coordenadas proporcionadas como parámetros
+	public static double distancia(Coordenada c1, Coordenada c2){
+	    double Distx = 0,Disty = 0, distancia;
+	    Distx = c1.getx()-c2.getx();
+	    Disty = c1.gety()-c2.gety();
+	   // Obtener valores absolutos
+	   if(Distx < 0){
+	      Distx=(Distx * -1);
+	    }
+	   if(Disty < 0){
+	      Disty = (Distx * -1);
+	     }
+	     distancia=Math.sqrt(Distx * Distx + Disty * Disty);
+	      return distancia;
+	}
+
+	// Método toString para representar la Coordenada en forma de texto
+	public String toString(){
+		return"("+ x + ";" + y + ")";
+	}
 }
---
+++ src/PrincipalMain.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+public class PrincipalMain {
+    // Método para calcular el área de intersección entre dos rectángulos
+    public static double rectanguloSobre(Rectangulo r1, Rectangulo r2) {
+        // Obteniendo las coordenadas de los rectángulos
+        Coordenada rect1Cord1 = r1.getEsquina1();
+        Coordenada rect1Cord2 = r1.getEsquina2();
+        Coordenada rect2Cord1 = r2.getEsquina1();
+        Coordenada rect2Cord2 = r2.getEsquina2();
+        // Coordenadas del rectángulo 1
+        double x1 = rect1Cord1.getx();  
+        double y1 = rect1Cord1.gety();
+        double x2 = rect1Cord2.getx();
+        double y2 = rect1Cord2.gety();
+        // Coordenadas del rectángulo 2
+        double x3 = rect2Cord1.getx();  
+        double y3 = rect2Cord1.gety();
+        double x4 = rect2Cord2.getx();
+        double y4 = rect2Cord2.gety();
+        // Calculando los puntos de intersección
+        double inicioXInterseccion = Math.max(x1, x3);
+        double finXInterseccion = Math.min(x2, x4);    
+        double inicioYInterseccion = Math.max(y4, y2);
+        double finYInterseccion = Math.min(y1, y3);
+        // Creando un nuevo rectángulo con los puntos de intersección
+        Coordenada c1 = new Coordenada(inicioXInterseccion, inicioYInterseccion);
+        Coordenada c2 = new Coordenada(finXInterseccion, finYInterseccion);
+        Rectangulo rectanguloInterseccion = new Rectangulo(c1, c2);
+        // Calculando el área de intersección
+        double areaSobrepuesta = rectanguloInterseccion.calculoArea();
+        return areaSobrepuesta;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i;
+        // Declarando las variables para las coordenadas
+        double x1, x2, x3, y3, y1, y2, x4, y4;
+        Coordenada c1, c2, c3, c4;
+        Rectangulo rec1, rec2;
+        // Pidiendo al usuario cuántos rectángulos desea mostrar
+        System.out.println("¿Cuántos rectángulos desea mostrar?");
+        int n = scanner.nextInt();
+        ContainerRect cr = new ContainerRect(n * 2);
+        for (i = 0; i < n; i++) {
+            // Pidiendo al usuario las coordenadas de los rectángulos
+            System.out.println("Ingrese una esquina del primer rectángulo: ");
+            x1 = scanner.nextDouble();
+            y1 = scanner.nextDouble();
+            System.out.println();
+            System.out.println("Ingrese la esquina opuesta del primer rectángulo: ");
+            x2 = scanner.nextDouble();
+            y2 = scanner.nextDouble();
+            System.out.println();
+            System.out.print("Ingrese una esquina del segundo rectángulo: ");
+            x3 = scanner.nextDouble();
+            y3 = scanner.nextDouble();
+            System.out.println();
+            System.out.println("Ingrese la esquina opuesta del segundo rectángulo: ");
+            x4 = scanner.nextDouble();
+            y4 = scanner.nextDouble();
+            // Creando los rectángulos con las coordenadas ingresadas
+            rec1 = new Rectangulo(new Coordenada(x1, y1), new Coordenada(x2, y2));
+            rec2 = new Rectangulo(new Coordenada(x3, y3), new Coordenada(x4, y4));
+            // Mostrando los rectángulos creados
+            System.out.println("Rectángulo A = " + rec1.toString());
+            System.out.println("Rectángulo B = " + rec2.toString());
+            // Añadiendo los rectángulos al contenedor
+            cr.addRectagulo(rec1);
+            cr.addRectagulo(rec2);
+            // Verificando si los rectángulos se interceptan
+            Verificador ver = new Verificador(rec1, rec2);
+            System.out.println(ver.verificar(rec1, rec2));
+        }
+        System.out.println();
+        // Mostrando todos los rectángulos en el contenedor
+        System.out.println(cr.toString());
+    }
+}
